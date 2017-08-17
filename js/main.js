e426bfe250a273f609f70c54d15e2487846fbc99
Kathleen <kathleenmcclay@gmail.com>
2017-08-17 19:43:56 -0400

    question 4 complete

--- js/main.js
+++ js/main.js
@@ -11,26 +11,20 @@ sign_in.addEventListener('click', function(){
   var modal = document.querySelector('.modal').style.display = "block"
 
 
-
-
-
-
-
-// When the user presses the .close button, hide the modal window
-
 button.addEventListener('click', function(){
   form.style.display = "none"
 })
 
-
-
-
-
-// When the user presses the .submit button, add an .error class to both input elements
 error.addEventListener('click', function(){
 user.classList.add('error')
 pass.classList.add('error')
 })
+
+error.addEventListener('mouseover', function(){
+  user.remove()
+})
+
+
 
 
 
